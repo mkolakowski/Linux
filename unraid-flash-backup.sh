cde06b5a6db61e693d217020b8a4bd3436c8fb05
mkolakowski <31713098+mkolakowski@users.noreply.github.com>
2021-07-19 14:15:41 -0400

    Update unraid-flash-backup.sh

--- unraid-flash-backup.sh
+++ unraid-flash-backup.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+#Script created and maintained by Mkolakowski at https://github.com/mkolakowski/Linux
+#No Warranty when using script or other components, Use at your own risk.
+
 #--sn = Server Name
 sn=$(hostname)
 echo $sn
 
-#--unraidver =  Version number of server
-unraidver=$(grep "emhttpd: Unraid(tm) System Management Utility version " /var/log/syslog | cut -c87-)
-echo $unraidver
+#--unraidversion =  Version number of server
+unraidversion=$(grep "emhttpd: Unraid(tm) System Management Utility version " /var/log/syslog | cut -c87-)
+echo $unraidversion
 
 #-- starttime = Date+Time Script Started, eg: 20190430-145150
 starttime=$(date +%Y%m%d-%H%M%S)
@@ -16,7 +19,7 @@ backuppath=/mnt/user/Backup-Unraid/$(date +%Y-%m)
 echo $backuppath
 
 $-- Name of backup Zip, Combines three above variables
-backupzip=$backuppath/flash-backup--$sn--$unraidver--$starttime.zip
+backupzip=$backuppath/flash-backup--$sn--$unraidversion--$starttime.zip
 echo $backupzip
 
 #-- Creating Local Directory for backup
