255040082ecf8e641b9763f64558e9cdfc738e79
mkolakowski <31713098+mkolakowski@users.noreply.github.com>
2021-07-19 15:42:29 -0400

    Documentation

--- unraid-flash-backup.sh
+++ unraid-flash-backup.sh
@@ -1,31 +1,41 @@
 #!/bin/bash
 
-#Script created and maintained by Mkolakowski at https://github.com/mkolakowski/Linux
-#No Warranty when using script or other components, Use at your own risk.
+# .SYNOPSIS 
+#   Script created and maintained by Mkolakowski at https://github.com/mkolakowski/Linux
+#   No Warranty when using script or other components, Use at your own risk.
+#
+# .VARIABLES
+#   backuppath     = Path to folder that will hold backups
+#   backupzip      = Name of backup Zip
+#   sn             =  Is the hostname of the Unraid Server
+#   starttime      = Date+Time Script Started, eg: 20190430-145150
+#   unraidversion  =  Version number of server
+#
+# .EXAMPLES
+#   Run script as is after validating data in above variables is correct
+#
+# .DEPENDANCIES
+#   1. Rclone must be installed and configured to point to remote
+#   2. Zip must be installed
+#   3. Unzip must be installed
+#
 
-#--sn = Server Name
 sn=$(hostname)
-echo $sn
-
-#--unraidversion =  Version number of server
 unraidversion=$(grep "emhttpd: Unraid(tm) System Management Utility version " /var/log/syslog | cut -c87-)
-echo $unraidversion
-
-#-- starttime = Date+Time Script Started, eg: 20190430-145150
 starttime=$(date +%Y%m%d-%H%M%S)
-
-#-- Path to folder that will hold backups
 backuppath=/mnt/user/Backup-Unraid/$(date +%Y-%m)
-echo $backuppath
-
-$-- Name of backup Zip, Combines three above variables
 backupzip=$backuppath/flash-backup--$sn--$unraidversion--$starttime.zip
-echo $backupzip
 
-#-- Creating Local Directory for backup
+#--Block Variable testing, remove the # before each line to enable
+#echo $sn
+#echo $unraidversion
+#echo $backuppath
+#echo $backupzip
+
+#--Creating Local Directory for backup
 mkdir $backuppath
 
-#--Creating Zip
+#--Creating Zip -- Note: you can change the backup parth to /boot/config if wanted
 zip -r $backupzip /boot
 
 #--Backing up Folder to rclone target
